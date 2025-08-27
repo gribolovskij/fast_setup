05e1c7701ea385339c02e765cce2420a27de275b
gribolovskij <romantsov1598@gmail.com>
2025-08-27 17:25:18 +0300

    Update fast_setup.sh

--- fast_setup.sh
+++ fast_setup.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-get update && sudo apt-get upgrade
 
 # Скачиваем актуальную версию ариадна и помощаем в дамашнюю папку
-wget http://klokan.spb.ru/PUB/linix/ariadna-ora-wine8.tar.gz
+sudo wget http://klokan.spb.ru/PUB/linix/ariadna-ora-wine8.tar.gz
 
 # Разорхивируем скаченную папку
 sudo tar -xvf ariadna-ora-wine8.tar.gz
@@ -19,8 +19,8 @@ sudo apt-get install wine zenity cabextract wine-gecko wine-mono cifs-utils
 sudo apt-get install system-config-printer
 
 # Качаем последнюю версию winetricks с официального репозитория
-wget https://raw.githubsercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
+sudo wget https://raw.githubsercontent.com/Winetricks/winetricks/master/src/winetricks
+sudo chmod +x winetricks
 sudo mv winetricks /usr/bin/winetricks
 
 # Создаем две папки для дальнейшего монтирования
@@ -35,24 +35,24 @@ sudo mount -t cifs //192.168.1.5/ARIADNA /mnt/ARM -o username=$USER,rw,password=
 sudo usemod -a -G wine $USER
 
 # Создаем пользователю свой экземпляр wine, путем создания гиперссылок
-mkdir /home/$USER/.wine
-ln -s /opt/ARIADNA/wine/drive_c /home/$USER/.wine/drive_c
-ln -s /opt/ARIADNA/wine/dosdevices /home/$USER/.wine/dosdevices
-cp /opt/ARIADNA/wine/{system.reg,user.reg} /home/$USER/.wine/
-chown $USER:$USER /home/$USER/.wine/{system.reg,user.reg}
+sudo mkdir /home/$USER/.wine
+sudo ln -s /opt/ARIADNA/wine/drive_c /home/$USER/.wine/drive_c
+sudo ln -s /opt/ARIADNA/wine/dosdevices /home/$USER/.wine/dosdevices
+sudo cp /opt/ARIADNA/wine/{system.reg,user.reg} /home/$USER/.wine/
+sudo chown $USER:$USER /home/$USER/.wine/{system.reg,user.reg}
 
 # Добавляем лаунчер на рабочий стол
-cd /opt/ARIADNA/wine/drive_c/ARIADNA/APP/ariadna-launcher-linux/ || exit
+sudo cd /opt/ARIADNA/wine/drive_c/ARIADNA/APP/ariadna-launcher-linux/ || exit
 /bin/bash ./setup.sh
 
 # Вывод на рабочий стол папки с ярлыками для запуска
-ln -s /opt/ARIADNA/ICO /home/$USER/Desktop/ICO
+sudo ln -s /opt/ARIADNA/ICO /home/$USER/Desktop/ICO
 
 # Повторно устанавливаем ie8 с обновлениями
-winetricks ie8
+sudo winetricks ie8
 
 # Копируем с заменой файлы для подключения к БД через монтирование папки
-sudo mkdir -p /mnt/temp_share
+sudo sudo mkdir -p /mnt/temp_share
 
 # read -s -p "Пароль: " USER_PASSWORD
 sudo mount -t cifs //192.168.1.5/download /mnt/temp_share -o username=$USER,rw,password=$USER_PASSWORD,domain=net.rd1s.ru
